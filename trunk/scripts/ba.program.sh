b96e363146b705fbe02828ba49c5dd1effacf4ca
Mike Kalmbach <mikekalmbach@gmail.com>
2016-05-02 16:55:31 -0500

    convert echo statements to log entries in ba.program.sh (and corrected spacing)

--- trunk/scripts/ba.program.sh
+++ trunk/scripts/ba.program.sh
@@ -35,15 +35,13 @@ done
 
 if [ -z "$tool_info" ]
 then
-	echo "TOOL FILE DOESNT EXIST! : $tool_info"
-	echo "VCF=$CWD_VCF"
-	ls -lhrt $CWD_VCF
+	echo "ERROR: The tool info file is a required parameter."
 	exit 100
 fi
 source "$tool_info"
 source "$MEM_INFO"
 source ${DIR}/shared_functions.sh
-
+source ${BIOR_ANNOTATE_DIR}/utils/log.sh
 if [[ -z "$CWD_VCF" || ! -e "$CWD_VCF" || ! -s "$CWD_VCF" ]]
 then
    ${DIR}/email.sh -f \$CWD_VCF -m ba.program.sh -M "VCF file does not exist" -p \$VCF -l \$LINENO
@@ -73,36 +71,41 @@ START_NUM=`egrep -v "^#|^$" $CWD_VCF |wc -l|cut -f1 -d" "`
 
 if [ "$runsnpeff" == "TRUE" ]
 then
-  if [ -z "$snpeff_mem" ]; then echo "CONFIGURATION Missing <snpeff_mem> Variable" >> ../err.log; fi
-  echo "Running SNPeff"
-	$JAVA7/java -Xmx$snpeff_mem -jar $SNPEFF/snpEff.jar $SNPEFF_params -c $SNPEFF/snpEff.config $SNPEFF_DB $CWD_VCF > $CWD_VCF.tmp
-	$PERL $SNPEFF_PARSE  $CWD_VCF.tmp > $CWD_VCF
-	END_NUM=`grep -v "^#" ${CWD_VCF}|wc -l|cut -f1 -d" "`
-	if [ ! "$END_NUM" -ge "$START_NUM" ];
-	then 
-		echo "${CWD_VCF} SnpfEFF Failed" >> ../err.log
-    	exit 100
-	fi
+  if [ -z "$snpeff_mem" ]
+  then 
+    log_error "SNP-Effect: memory_info does not specify <snpeff_mem> variable" 
+  fi
+
+  log_dev "Running SNPeff"
+  $JAVA7/java -Xmx$snpeff_mem -jar $SNPEFF/snpEff.jar $SNPEFF_params -c $SNPEFF/snpEff.config $SNPEFF_DB $CWD_VCF > $CWD_VCF.tmp
+  $PERL $SNPEFF_PARSE  $CWD_VCF.tmp > $CWD_VCF
+  END_NUM=`grep -v "^#" ${CWD_VCF}|wc -l|cut -f1 -d" "`
+  if [ ! "$END_NUM" -ge "$START_NUM" ];
+  then 
+    log_error "Detected failure in ${CWD_VCF}: Started with $START_NUM lines, ended with $END_NUM" 
+    exit 100
+  fi
 fi
 
 if [ "$runCAVA" == "TRUE" ]
 then
-  echo "Running CAVA"
-	$PYTHON/python $CAVA -c $CAVA_CONFIG -i $CWD_VCF -o $CWD_VCF.tmp
-	if [ ! -f $CWD_VCF.tmp.vcf ]; then
-		echo "CAVA FAILED TO RUN with tthe following command"
-		echo "$PYTHON/python $CAVA -c $CAVA_CONFIG -i $CWD_VCF -o $CWD_VCF.tmp"
-		exit 100
-	fi
-	cat $CWD_VCF.tmp.vcf |$PERL $CAVA_PARSE - |$PERL -pne 's/\s\n/\n/' > $CWD_VCF.tmp2.vcf
-	mv $CWD_VCF.tmp2.vcf $CWD_VCF.tmp.vcf
-	END_NUM=`egrep -v "^#|^$" ${CWD_VCF}.tmp.vcf |wc -l|cut -f1 -d" "`
-	if [ ! "$END_NUM" -ge "$START_NUM" ];
-	then 
-		echo  "${CWD_VCF}. CAVA has insufficient number of rows. CAVA Failed" >> ../err.log;
-    	exit 100;
-	fi
-	mv $CWD_VCF.tmp.vcf $CWD_VCF
+  log_dev "Running CAVA"
+  $PYTHON/python $CAVA -c $CAVA_CONFIG -i $CWD_VCF -o $CWD_VCF.tmp
+  if [ ! -f $CWD_VCF.tmp.vcf ]; then
+    log_error "CAVA FAILED TO RUN with the following command"
+    log_error "$PYTHON/python $CAVA -c $CAVA_CONFIG -i $CWD_VCF -o $CWD_VCF.tmp"
+    exit 100
+  fi
+
+  cat $CWD_VCF.tmp.vcf |$PERL $CAVA_PARSE - |$PERL -pne 's/\s\n/\n/' > $CWD_VCF.tmp2.vcf
+  mv $CWD_VCF.tmp2.vcf $CWD_VCF.tmp.vcf
+  END_NUM=`egrep -v "^#|^$" ${CWD_VCF}.tmp.vcf |wc -l|cut -f1 -d" "`
+  if [ ! "$END_NUM" -ge "$START_NUM" ];
+  then 
+    log_error  "${CWD_VCF}. CAVA has insufficient number of rows (expected at least $START_NUM, but found only $END_NUM). CAVA Failed"
+    exit 100
+  fi
+  mv $CWD_VCF.tmp.vcf $CWD_VCF
 fi
 
 
@@ -111,56 +114,61 @@ fi
 #IF PEDIGREE is specified, and AF, then you can run
 if [ ! -z "$PEDIGREE" ]
 then
-	echo "PEDIGREE = $PEDIGREE"
-	if [ ! -z "GENE_LIST" ]
-	then
-		GENE_LIST=" -g $GENE_LIST "
-	fi
-	if [[ $DRILLS == *ExAC* ]]; 
-	then 
-		echo "$PERL $INHERITANCE -v $CWD_VCF -p $PEDIGREE $GENE_LIST|$PERL $COMPOUNDHET -v - -p $PEDIGREE >  $CWD_VCF.tmp"
-		$PERL $INHERITANCE -v $CWD_VCF -p $PEDIGREE $GENE_LIST|$PERL $COMPOUNDHET -v - -p $PEDIGREE >  $CWD_VCF.tmp
-		END_NUM=`egrep -v "^#|^$" ${CWD_VCF}.tmp |wc -l|cut -f1 -d" "`
-		if [ ! "$END_NUM" -ge "$START_NUM" ];
-		then 
-		echo "${CWD_VCF} Inheritance script Failed" >> ../../err.log
-    	exit 1
-		fi
-	mv $CWD_VCF.tmp $CWD_VCF
-	fi
+  log_debug "PEDIGREE = $PEDIGREE"
+  if [ ! -z "GENE_LIST" ]
+  then
+    GENE_LIST=" -g $GENE_LIST "
+  fi
+
+  if [[ $DRILLS == *ExAC* ]]; 
+  then 
+    echo "$PERL $INHERITANCE -v $CWD_VCF -p $PEDIGREE $GENE_LIST|$PERL $COMPOUNDHET -v - -p $PEDIGREE >  $CWD_VCF.tmp"
+    $PERL $INHERITANCE -v $CWD_VCF -p $PEDIGREE $GENE_LIST|$PERL $COMPOUNDHET -v - -p $PEDIGREE >  $CWD_VCF.tmp
+    END_NUM=`egrep -v "^#|^$" ${CWD_VCF}.tmp |wc -l|cut -f1 -d" "`
+    if [ ! "$END_NUM" -ge "$START_NUM" ];
+    then 
+      log_error "${CWD_VCF} Inheritance script failed--expected at least $START_NUM lines, but only found $END_NUM" 
+      exit 100
+    fi
+    mv $CWD_VCF.tmp $CWD_VCF
+  fi
 fi
 
 #Add VCF Links for VCF miner
 if [ -z "$LINKOFF" ]
 then
-	$PERL $LINKS -v $CWD_VCF | $PERL $TRIM -v - -o BaseQRankSum,ClippingRankSum,DS,END,FS,HaplotypeScore,InbreedingCoeff,MLEAC,MLEAF,MQRankSum,NEGATIVE_TRAIN_SITE,POSITIVE_TRAIN_SITE,QD,ReadPosRankSum,SOR,VQSLOD,set  > $CWD_VCF.tmp
-		END_NUM=`egrep -v "^#|^$" ${CWD_VCF}.tmp |wc -l|cut -f1 -d" "`
-		if [ ! "$END_NUM" -ge "$START_NUM" ];
-		then 
-		echo "${CWD_VCF} Add Links script Failed" >> ../../err.log
-		exit 100
-		fi	
-	mv $CWD_VCF.tmp $CWD_VCF
+  $PERL $LINKS -v $CWD_VCF | $PERL $TRIM -v - -o BaseQRankSum,ClippingRankSum,DS,END,FS,HaplotypeScore,InbreedingCoeff,MLEAC,MLEAF,MQRankSum,NEGATIVE_TRAIN_SITE,POSITIVE_TRAIN_SITE,QD,ReadPosRankSum,SOR,VQSLOD,set  > $CWD_VCF.tmp
+  END_NUM=`egrep -v "^#|^$" ${CWD_VCF}.tmp |wc -l|cut -f1 -d" "`
+  if [ ! "$END_NUM" -ge "$START_NUM" ];
+  then 
+    log_error "${CWD_VCF} Add Links script failed--expected at least $START_NUM lines, but only found $END_NUM" 
+    exit 100
+  fi	
+  mv $CWD_VCF.tmp $CWD_VCF
 fi
 
 editLabel="bior\.\.|INFO\.|Info\.|bior\."
 #Print out table
 if [ "$table" != 0 ]
 then
-echo "Making table file"
-echo "$DRILLS"|tr ";" "\n"|awk '($1)' > drill.table	
-	if [ "$runsnpeff" == "TRUE" ]
-	then 
-		echo -e "snpeff.Gene_name\nsnpeff.Amino_acid_change\nsnpeff.Transcript\nsnpeff.Exon\nsnpeff.Effect\nsnpeff.Effect_impact\nsnpeff.Amino_acid_change\nsnpeff.Codon_change" >> drill.table
-	fi
-	if [ "$runCAVA" == "TRUE" ];
-	then 
-		echo -e "CAVA_IMPACT\nCAVA_TYPE\nCAVA_GENE\nCAVA_ENST\nCAVA_TRINFO\nCAVA_LOC\nCAVA_CSN\nCAVA_CLASS\nCAVA_SO\nCAVA_ALTFLAG\nCAVA_ALTANN\nCAVA_ALTCLASS\nCAVA_DBSNP" >> drill.table
-	fi
-	echo "Drilling into the data"
-	#cat drill.table
-	echo "#######################################################"
-	#$PERL $INFO_PARSE $CWD_VCF -q "${DRILLS}" |grep -v "^##"|perl -pne 's/$editLabel//g' > $CWD_VCF.xls
+  log "Making table file"
+  echo "$DRILLS"|tr ";" "\n"|awk '($1)' > drill.table	
+  if [ "$runsnpeff" == "TRUE" ]
+  then 
+    echo -e "snpeff.Gene_name\nsnpeff.Amino_acid_change\nsnpeff.Transcript\nsnpeff.Exon\nsnpeff.Effect\nsnpeff.Effect_impact\nsnpeff.Amino_acid_change\nsnpeff.Codon_change" >> drill.table
+  fi
+
+  if [ "$runCAVA" == "TRUE" ];
+  then 
+    echo -e "CAVA_IMPACT\nCAVA_TYPE\nCAVA_GENE\nCAVA_ENST\nCAVA_TRINFO\nCAVA_LOC\nCAVA_CSN\nCAVA_CLASS\nCAVA_SO\nCAVA_ALTFLAG\nCAVA_ALTANN\nCAVA_ALTCLASS\nCAVA_DBSNP" >> drill.table
+  fi
+
+  log_debug "Drilling into the data"
+
+  ## TODO: Why is this here? Did this functionality move?
+  #cat drill.table
+  echo "#######################################################"
+  #$PERL $INFO_PARSE $CWD_VCF -q "${DRILLS}" |grep -v "^##"|perl -pne 's/$editLabel//g' > $CWD_VCF.xls
 fi    
 #Final files $CWD_VCF & possibly $CWD_VCF.savant.xls
 
