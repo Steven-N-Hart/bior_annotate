0ec0f6f1db931d3bce1deaf9eac8bcd4d9318c05
Mike Kalmbach <mikekalmbach@gmail.com>
2016-05-02 15:48:25 -0500

    Use logger instead of echo statements in ba.merge.

--- trunk/scripts/ba.merge.sh
+++ trunk/scripts/ba.merge.sh
@@ -36,6 +36,7 @@ done
 
 source $tool_info
 source ${DIR}/shared_functions.sh
+source ${BIOR_ANNOTATE_DIR}/utils/log.sh
 
 if [ ! -z "$LOG" ] 
 then 
@@ -47,23 +48,22 @@ fi
 ###     Setup configurations
 ###
 ##################################################################################
-#echo "using the script in $DIR"
 #Count to make sure there are at least as many variants in the output as were in the input
 for x in $CREATE_DIR/*anno
 do
 PRECWD_VCF=${x/.anno/}
-#echo "x=$x and PRECWD_VCF=$PRECWD_VCF"
+
 if [ -z "$PRECWD_VCF"  ];
 then
-        echo "Can't find PRECWD_VCF=$PRECWD_VCF ";
+        log_error "Can't find PRECWD_VCF=$PRECWD_VCF ";
         exit 100;
 fi
 START_NUM=`cat $PRECWD_VCF|grep -v "^#"|wc -l|cut -f1 -d" "`
 END_NUM=`cat $x|grep -v "^#"|wc -l|cut -f1 -d" "`
 if [ ! "$END_NUM" -ge "$START_NUM" ];
 then 
-        echo "$x has insufficient number of rows to be considered complete";
-        echo "PRECWD_VCF=$PRECWD_VCF START_NUM=$START_NUM END_NUM=$END_NUM";
+        log_error "$x has insufficient number of rows to be considered complete";
+        log_error "PRECWD_VCF=$PRECWD_VCF START_NUM=$START_NUM END_NUM=$END_NUM";
         exit 100;
 fi
 if [ ${PRECWD_VCF: -3} == "000" ]
@@ -82,16 +82,6 @@ done
 
 if [ "$table" != "0" ]
 then
-	### create the config file for parser to create the XLS file from a VCF file
-	#for catalog in `cat $drill | cut -f1`
-	#do
-	#	for annot in `cat $drill | grep -w $catalog | cut -f2 | tr "," " "`
-	#	do
-	#		echo "$catalog.$annot" >> vcf2xls.config.txt
-	#	done
-	#done
-	#$PERL $DIR/bior_vcf2xls.pl -i ${outname}.vcf -o ${outname}.xls -c vcf2xls.config.txt   	
-	#mv ${outname}.xls ../
 	if [ "$table" == 1 ]
 	then
 		$PERL $DIR/bior_vcf2xls.pl -i ${outname}.vcf -o ${outname}.xls -c $CREATE_DIR/drill.table
@@ -108,7 +98,7 @@ BEGINNING=`grep -v "^#" ${outname}.vcf|uniq|wc -l`
 FINAL=`zcat  ${outname}.vcf.gz|grep -v "^#"|wc -l`
 if [ $FINAL -lt $BEGINNING ]
 then
-	echo "ERROR! Compression failed"
+	log_error "ERROR! Compression failed - only found $FINAL variants, expected $BEGINNING"
 	exit 100
 fi
 rm ${outname}.vcf
