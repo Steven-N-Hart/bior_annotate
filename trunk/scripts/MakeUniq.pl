b9589a6d438cc46f9ad56b97c23ac60481dbbe75
M087494 <hart.steven@mayo.edu>
2016-11-01 15:54:42 -0500

    Fixed duplicate issue

--- trunk/scripts/MakeUniq.pl
+++ trunk/scripts/MakeUniq.pl
@@ -1,12 +1,14 @@
 #!/usr/bin/env perl
 BEGIN{$chr="chr";$pos=0;$ref="N";$alt="N";$count=0};
 while (<>){
+	if($_=~/^#/){print;next}
 	chomp; 
 	s/\t\n/\n/;
 	s/==/=/g;
 	s/\t;/\t/;
 	($CHR,$POS,$ID,$REF,$ALT,@LINE)=split("\t",$_);
-	next if($CHR==$chr && $POS==$pos && $REF eq $ref && $ALT eq $alt &&!(eof)); 
+	#print STDERR "$CHR==$chr && $POS==$pos && $REF eq $ref && $ALT eq $alt &&!(eof)\n";
+	next if($CHR==$chr && $POS==$pos && $REF eq $ref && $ALT eq $alt); 
 	$chr=$CHR;$pos=$POS;$ref=$REF;$alt=$ALT; 
 	print "$_\n";$count++
 }
